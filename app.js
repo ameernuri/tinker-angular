63c99e26961d07d19278c1cee163c0a5320dffd4
Ameer Nuri <ameernuri@gmail.com>
2015-12-02 06:45:42 +0300

    basic setups

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var app = express();
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+var lessMiddleware = require('less-middleware');
+
+app.use(lessMiddleware(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.render('app.html');
 });
 
 var server = app.listen('3000', function() {
-	console.log('Listening on port 3000')
+	console.log('Listening on port 3000');
 });
